c942dfc675355fc9a987a46b9ddaa54ab5bc80fc
Gabesz Rozsa <gabesz.rozsa@gmail.com>
2019-07-27 18:51:04 +0200

    Pagination

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,11 @@
 import Items from "../components/Items";
 
-const Home = () => {
-  return <Items />;
+const Home = props => {
+  return (
+    <div>
+      <Items page={parseFloat(props.query.page) || 1} />
+    </div>
+  );
 };
 
 export default Home;
